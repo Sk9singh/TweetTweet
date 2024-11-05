07688abeb08799fff5c3703f612c1626efbbab02
Saksham Singh <sakshamsingh8278@gmail.com>
2024-11-06 02:58:45 +0530

    Added hashtags to tweets

--- src/services/tweet-service.js
+++ src/services/tweet-service.js
@@ -1,18 +1,26 @@
-const {TweetRepository} = require('../repository/index');
+const {TweetRepository,HashtagRepository} = require('../repository/index');
 
 class TweetService {
     constructor() {
         this.tweetRepository = new TweetRepository();
+        this.hashtagRepository = new HashtagRepository();
     }
 
    async create(data) {
         const content = data.content;
-        const tags = content.match(/#[a-zA-Z0-9_]+/g);  // regex extracts hashtags
-        tags = tags.map((tag) => tag.substring(1));
-        console.log(tags);
+        const tags = content.match(/#[a-zA-Z0-9_]+/g).map((tag) => tag.substring(1));  // regex extracts hashtags
 
         const tweet = await this.tweetRepository.create(data);
-        return tweet;
+        let alreadyPresentTags = await this.hashtagRepository.findByName(tags);
+        alreadyPresentTags = alreadyPresentTags.map(tags => tags.title); 
+
+        let newTags = tags.filter(tag => !alreadyPresentTags.includes(tag));
+        newTags = newTags.map(tag => {
+            return {title: tag, tweets: [tweet.id]}
+        });
+        const response = await this.hashtagRepository.bulkCreate(newTags);
+
+        return tweet;  
     }
 
 }

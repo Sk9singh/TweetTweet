0b360d462deab3074ed2a27316b30b346c6df4e3
Saksham Singh <sakshamsingh8278@gmail.com>
2024-11-07 13:11:03 +0530

    Added controller and router

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import { connect } from './config/database.js';
+import apiRoutes from './routes/index.js';
 
 const app = express();
 
-import TweetService from './services/tweet-service.js';
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/api',apiRoutes);
 
 app.listen(3000, async ()=> {
 
     console.log('server started at port 3000');
     await connect();
     console.log('Mongo db connected');
-    let service = new TweetService();
-    const tweet = await service.create({content: 'This is #feeling #good #great'});
-    console.log(tweet);
 }); 
--- src/services/tweet-service.js
+++ src/services/tweet-service.js
@@ -8,8 +8,7 @@ class TweetService {
 
    async create(data) {
         const content = data.content;
-        const tags = content.match(/#[a-zA-Z0-9_]+/g).map((tag) => tag.substring(1));  // regex extracts hashtags
-
+        const tags = content.match(/#[a-zA-Z0-9_]+/g).map((tag) => tag.substring(1).toLowerCase());  // regex extracts hashtags
         const tweet = await this.tweetRepository.create(data);
         let alreadyPresentTags = await this.hashtagRepository.findByName(tags);
         let titleOfPresentTags = alreadyPresentTags.map(tags => tags.title); 

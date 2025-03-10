8b709eb48aee8e5740f925794a4c830cd6de1a62
Saksham Singh <sakshamsingh8278@gmail.com>
2025-03-11 00:06:30 +0530

    Check

---
+++ src/services/like-service.js
@@ -0,0 +1,44 @@
+import {TweetRepository,LikeRepository} from '../repository/index.js';
+
+class LikeService {
+    constructor() {
+        this.likeRepository = new LikeRepository();
+        this.tweetRepository = new TweetRepository();
+    }
+
+    async toggleLike(modelId, modelType,userId) {  //--> api/v1/likes/toggle?id=modelId & type = tweet/comment
+         if (modelType == 'Tweet') {
+            var likeable = await this.tweetRepository.get(modelId);
+         } else if(modelType == 'Comment') {
+            //todo
+         } else {
+            throw new Error('Unknown model type');
+         }
+
+         const exists = await this.likeRepository.findByUserAndLikeable({
+            user: userId,
+            onModel: modelType,
+            likeable: modelId
+         });
+
+         if(exists) {
+            likeable.likes.pull(exists.id);
+            await likeable.save();
+            await exists.remove();
+            var isAdded = false;
+
+         } else {
+            const newLike = await this.likeRepository.create({
+                user: userId,
+                onModel: modelType,
+                likeable: modelId
+            });
+            likeable.likes.push(newLike);
+            await likeable.save();
+            var isAdded = true;
+         }
+         return isAdded;
+    }
+}
+
+export default LikeService;

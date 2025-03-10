301029c144582dde09b73657c14612df1f2fb315
Saksham Singh <sakshamsingh8278@gmail.com>
2025-03-10 23:54:03 +0530

    Revert "Testing likes"
    This reverts commit 1bea5c9cec5887115d8c1761c2b6440a17e7be79.

--- src/repository/index.js
+++ src/repository/index.js
@@ -1,9 +1,7 @@
 import TweetRepository from './tweet-repository.js';
 import HashtagRepository from './hashtag-repository.js';
-import LikeRepository from './like-repository.js';
 
 export {
     TweetRepository,
-    HashtagRepository,
-    LikeRepository
+    HashtagRepository
 }

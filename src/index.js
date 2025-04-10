677a47c2939042471b94064b33834f5bd16d4602
Saksham Singh <sakshamsingh8278@gmail.com>
2025-04-10 20:47:04 +0530

    Toggle likes added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import bodyParser from 'body-parser';
 import { connect } from './config/database.js';
 import apiRoutes from './routes/index.js';
 
+import UserRepository from './repository/user-repository.js';
+import TweetRepository from './repository/tweet-repository.js';
+import LikeService from './services/like-service.js';
+
 const app = express();
 
 app.use(bodyParser.json());
@@ -15,4 +19,13 @@ app.listen(3000, async ()=> {
     console.log('server started at port 3000');
     await connect();
     console.log('Mongo db connected');
+
+
+    const userRepo = new UserRepository();
+    const tweetRepo = new TweetRepository();
+    const tweets = await tweetRepo.getAll(0,10);
+    const users = await userRepo.getAll();
+    const likeService = new LikeService();
+    await likeService.toggleLike(tweets[0].id, 'Tweet', users[0].id);
+
 }); 
--- src/repository/tweet-repository.js
+++ src/repository/tweet-repository.js
@@ -16,7 +16,7 @@ class TweetRepository extends CrudRepository {
 
     async getWithComments(id) {
         try {
-            const tweet = await Tweet.findById(id).populate({path: 'comments'});
+            const tweet = await Tweet.findById(id).populate({path: 'comments'}).lean();
             return tweet;
            } catch (error) {
                console.log(error);
@@ -34,7 +34,7 @@ class TweetRepository extends CrudRepository {
 
     async find(id) {
         try {
-            const tweet = await Tweet.findById(id).populate({path:'likes'});
+            const tweet = await Tweet.findById(id).populate({path: 'likes'});
             return tweet;
             } catch (error) {
                console.log(error);

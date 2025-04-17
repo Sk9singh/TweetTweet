629b29c1e62f65f0cc53662c776227cab577f4ca
Saksham Singh <sakshamsingh8278@gmail.com>
2025-04-18 04:44:27 +0530

    auth error fixed

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { connect } from './config/database.js';
 import apiRoutes from './routes/index.js';
-
+import passport from 'passport';
+import {passportAuth} from './config/jwt-middleware.js';
 
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(passport.initialize());
+passportAuth(passport);
 
 app.use('/api',apiRoutes);
 
@@ -17,7 +21,5 @@ app.listen(3000, async ()=> {
     console.log('server started at port 3000');
     await connect();
     console.log('Mongo db connected');
-
-
 
 }); 

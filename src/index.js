c5280995382de6a448a38d7777c2f12aeb618dc1
Saksham Singh <sakshamsingh8278@gmail.com>
2025-04-18 02:32:21 +0530

    auth changes

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import bodyParser from 'body-parser';
 import { connect } from './config/database.js';
 import apiRoutes from './routes/index.js';
 
-// import UserRepository from './repository/user-repository.js';
-// import TweetRepository from './repository/tweet-repository.js';
-// import LikeService from './services/like-service.js';
+
 
 const app = express();
 
@@ -21,11 +19,5 @@ app.listen(3000, async ()=> {
     console.log('Mongo db connected');
 
 
-    // const userRepo = new UserRepository();
-    // const tweetRepo = new TweetRepository();
-    // const tweets = await tweetRepo.getAll(0,10);
-    // const users = await userRepo.getAll();
-    // const likeService = new LikeService();
-    // await likeService.toggleLike(tweets[0].id, 'Tweet', users[0].id);
 
 }); 
